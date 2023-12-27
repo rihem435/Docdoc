9f33cf9a39f30591b0458a19c4b50b3d11d98925
Rihem Zairi <rihem.zairi@gate-training.com>
2023-12-27 10:20:05 +0100

    add splash screen to android 12

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:app/core/routing/app_router.dart';
 import 'package:app/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
-  runApp( DocApp(appRouter: AppRouter(),));
+Future<void> main() async {
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
 }
-
